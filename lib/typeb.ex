a0a1ebf147f88dc63572a961ef7de928eb13ca69
torin <SecretSword2@users.noreply.github.com>
2019-07-16 13:46:52 +0900

    Initial commit

---
+++ lib/typeb.ex
@@ -0,0 +1,18 @@
+defmodule Typeb do
+  @moduledoc """
+  Documentation for Typeb.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Typeb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/typeb_test.exs
@@ -0,0 +1,8 @@
+defmodule TypebTest do
+  use ExUnit.Case
+  doctest Typeb
+
+  test "greets the world" do
+    assert Typeb.hello() == :world
+  end
+end
